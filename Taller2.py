ceb01cba20b01bef5e15de949c566e61771e4961
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-09-09 22:47:26 -0500

    Tercer punto

--- Taller2.py
+++ Taller2.py
@@ -46,3 +46,24 @@ else:
     total = totalCompra - descuento
     print(f"El 20% de descuento es de: ${descuento:,}")
     print(f"El total con un 15% de descuento es: ${total:,}")
+
+# 3. Una compañía de seguros está abriendo un departamento de
+# finanzas y estableció un programa para captar clientes, que conssite
+# en lo siguiente: Si el monto por el que se efectúa la fianza es menor
+# que $50.000 la cuota a pagar será por el 3% del monto, y si el monto
+# es mayor que $50.000 la cuota a pagar será el 2% del monto. La
+# afianzadora desea determinar cual será la cuota que debe pagar al
+# cliente.
+
+monto = float(input("Ingrese monto de la fianza: $"))
+
+if monto < 50000:
+    interes = monto * 0.03
+    monto_interes = monto + interes
+    print(f"El interés del 3% es: ${interes:,}")
+    print(f"La cuota con un 3% es: ${monto_interes:,}")
+else:
+    interes = monto * 0.02
+    monto_interes = monto + interes
+    print(f"El interés del 2% es: ${interes:,}")
+    print(f"La cuota con un 2% es: ${monto_interes:,}")
