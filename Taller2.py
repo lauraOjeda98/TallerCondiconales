a82a18ca90f5bf8bd1e5af40133c9ec47a825203
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-09-09 22:17:12 -0500

    Segundo punto

--- Taller2.py
+++ Taller2.py
@@ -4,6 +4,9 @@ Created on Thu Sep  9 11:58:14 2021
 
 @author: laura
 """
+
+import random
+
 # Taller condiconales
 
 # 1. Hacer un algoritmo que calcule el total a pagar por la compra de
@@ -12,7 +15,7 @@ Created on Thu Sep  9 11:58:14 2021
 # un descuento del 10%.
 
 num_camisas = int(input("Ingrese cantidad de camisas a comprar: "))
-valor_camisas = float(input("Ingrese valor por camisa: "))
+valor_camisas = float(input("Ingrese valor por camisa: $"))
 total = num_camisas*valor_camisas
 print(f"El total sin descuento es: ${total:,}")
 
@@ -22,3 +25,24 @@ if num_camisas >= 3:
 else:
     total = total - (total * 0.10)
     print(f"El total con un 10% de descuento es: ${total:,}")
+
+# 2. En un supermercado se hace una promoción, mediante la cual el
+# cliente obtiene un descuento dependiendo de un número que se
+# escoge al azar. Si el número escogido es menor que 74 el descuento
+# es del 15% sobre el total de la compra, si es mayor o igual a 74 el
+# descuento es del 20%. Obtener cuanto dinero se le descuenta.
+
+num_aleatorio = random.randint(0, 100)
+totalCompra = float(input("Ingrese total de la compra: $"))
+print(f"El número aleatorio es: {num_aleatorio}")
+
+if num_aleatorio >= 74:
+    descuento = totalCompra * 0.2
+    total = totalCompra - descuento
+    print(f"El 20% de descuento es de: ${descuento:,}")
+    print(f"El total con un 20% de descuento es: ${total:,}")
+else:
+    descuento = totalCompra * 0.15
+    total = totalCompra - descuento
+    print(f"El 20% de descuento es de: ${descuento:,}")
+    print(f"El total con un 15% de descuento es: ${total:,}")
